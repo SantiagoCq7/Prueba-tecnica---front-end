76f6d30054a0f46c971cddc3f7121abd77a44970
Andrés Santiago Castillo Quintero <130021511+SantiagoCq7@users.noreply.github.com>
2026-01-04 23:10:16 -0500

    Update useDebounce.ts

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 
 // Hook personalizado para retrasar la actualización de un valor
-// Útil para evitar muchas llamadas a la API mientras el usuario escribe en un buscador
 export function useDebounce<T>(value: T, delay: number): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
